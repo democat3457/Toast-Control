e60eb2605f913bcf81cbfd4de265cca5d56cc0d4
Colin Wong <cwongmath@gmail.com>
2021-02-09 19:48:57 +0000

    Version bump

--- src/main/java/shadows/toaster/ToastControl.java
+++ src/main/java/shadows/toaster/ToastControl.java
@@ -30,7 +30,7 @@ public class ToastControl {
 
 	public static final String MODID = "toastcontrol";
 	public static final String MODNAME = "Toast Control";
-	public static final String VERSION = "1.8.1";
+	public static final String VERSION = "1.8.2";
 	public static final String DEPS = "required-after:placebo@[1.6,)";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
